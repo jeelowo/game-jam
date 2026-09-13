5770f45dbb51f82477f9c302c6a74983a6afe030
jeelowo <plegariajamuel47@gmail.com>
2026-09-13 16:49:02 +0800

    fix: camera look ahead now does not work diagonally

--- Scripts/player_manager.gd
+++ Scripts/player_manager.gd
@@ -7,6 +7,7 @@ extends Node2D
 
 var camera_distance := 0.0
 @export var max_camera_distance := 100.0
+var look_direction : Vector2
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("flashlight"):
@@ -15,9 +16,24 @@ func _process(_delta: float) -> void:
 	if flashlight.enabled:
 		flashlight.rotation = walk.last_facing_direction.angle()
 
-	look_ahead(walk.last_facing_direction)
+	if walk.last_facing_direction == Vector2.UP or Input.is_action_just_pressed("move_up"):
+		look_direction = Vector2.UP
+	elif walk.last_facing_direction == Vector2.DOWN or Input.is_action_just_pressed("move_down"):
+		look_direction = Vector2.DOWN
+	elif walk.last_facing_direction == Vector2.LEFT:
+		look_direction = Vector2.LEFT
+	elif walk.last_facing_direction == Vector2.RIGHT:
+		look_direction = Vector2.RIGHT
+
+	look_ahead(look_direction)
 
 func look_ahead(direction):
 	var camera_pos = player.global_position + (direction * max_camera_distance)
 	var camera_direction = camera_2d.global_position.direction_to(camera_pos).normalized()
 	camera_2d.global_position += camera_direction * float(sqrt(abs(camera_2d.global_position.distance_to(camera_pos)))/2)
+
+func look_up_or_down():
+	if Input.is_action_just_pressed("move_up"):
+		look_direction = Vector2.UP
+	elif Input.is_action_just_pressed("move_down"):
+		look_direction = Vector2.DOWN
