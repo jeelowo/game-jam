4e3345b25329d107f96f28de3f7528b2acb5b559
jeelowo <plegariajamuel47@gmail.com>
2026-09-15 14:05:32 +0800

    feat: reset camera after idling for 4 secs

--- Scripts/idle state.gd
+++ Scripts/idle state.gd
@@ -3,9 +3,13 @@ class_name playerIdle
 
 @onready var walk: playerWalk = $"../Walk"
 @onready var player_sprite: AnimatedSprite2D = $"../../Player Sprite"
+@onready var idle_timer: Timer = $"../../Idle Timer"
+@export var camera_time := 4.0
 
 func Enter():
 	print("State: " + self.name)
+	idle_timer.start(camera_time)
+	idle_timer.paused = false
 
 	# player face to last walk direction
 	if walk.last_facing_direction == Vector2.UP:
@@ -22,7 +26,10 @@ func Enter():
 		player_sprite.flip_h = true
 
 func Physics_Update(_delta: float):
-
+	print(idle_timer.time_left)
 	# Transitions
 	if Input.get_vector("move_left","move_right","move_up","move_down") != Vector2(0,0):
 		Transitioned.emit(self, "Walk")
+
+func Exit():
+	idle_timer.paused = true
--- Scripts/player_manager.gd
+++ Scripts/player_manager.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var walk: playerWalk = $"../State Machine/Walk"
 @onready var player: CharacterBody2D = $".."
 @onready var camera_2d: Camera2D = $"../Camera2D"
+@onready var idle_timer: Timer = $"../Idle Timer"
 
 var camera_distance := 0.0
 @export var max_camera_distance := 100.0
@@ -26,6 +27,7 @@ func _process(_delta: float) -> void:
 		look_direction = Vector2.RIGHT
 
 	look_ahead(look_direction)
+	camera_reset()
 
 func look_ahead(direction):
 	var camera_pos = player.global_position + (direction * max_camera_distance)
@@ -37,3 +39,8 @@ func look_up_or_down():
 		look_direction = Vector2.UP
 	elif Input.is_action_just_pressed("move_down"):
 		look_direction = Vector2.DOWN
+
+func camera_reset():
+	if idle_timer.time_left <= 0.0 and not idle_timer.paused and \
+	player.global_position.distance_to(camera_2d.global_position) > 5:
+		camera_2d.global_position += (-1*(look_direction)) * 4
